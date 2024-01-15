6a7472e5663b80375d548205af17b99f4a1fc7d7
Your Name <you@example.com>
2024-01-15 11:36:09 +0000

    trying to add error handling

--- app/errorhandler.js
+++ app/errorhandler.js
@@ -1,8 +1,11 @@
 
-// exports.errorHandler = (err,req,res,next) => {
-//     //checking for bad requests or invalid ids etc. 
-//     if (){}
-//     else{
-//         next(err)
-//     }
-// }
+
+exports.errorHandler = (err,req,res,next) => {
+    //checking for bad requests or invalid ids etc. 
+    if (err.code === "23502" || err.code === "22P02"){
+         return res.status(400).send({msg:"Bad request"})
+    }
+    else{
+        return res.status(500).send({msg: "generic bad error"})
+    }
+};
