3401d6219b7ade0c14ec4619cb9cfb478c691656
Your Name <you@example.com>
2024-01-15 14:06:38 +0000

    add first ideas for get /api

--- app/app.test.js
+++ app/app.test.js
@@ -3,6 +3,7 @@ const request = require('supertest')
 const db = require('../db/connection.js')
 const seed = require('../db/seeds/seed.js')
 const {articleData, commentData, topicData, userData} = require('../db/data/test-data/index.js')
+const endpoints = require('../endpoints.json')
 
 
 
@@ -17,7 +18,7 @@ describe('GET',() => {
             .get('/api/topics')
             .expect(200)
             .then((response) => {
-                console.log(response)
+                // console.log(response)
                response.body.topics.forEach((topic) => {
                 //testing for a truthy value to see if these properties exist
                 expect(typeof topic.description).toBe("string")
@@ -25,10 +26,20 @@ describe('GET',() => {
                })
             })
         });
+  })
+})
+
+describe('GET',() => {
+    describe('/api', () => {
+        test('return with an object describing all endpoints available ', async () => {
+            const response = await request(app).get('/api')
+            expect(200)
+            expect(response.body).toEqual(endpoints)
+        });
   
-        test('Test Case 2', () => {
- 
-     });
+        //  struggling on how to implement error handling  
   
   })
-});
+})
+
+
